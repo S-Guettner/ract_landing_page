b3ed48cb91d4fd98544f16919d3060b6b5d09ff8
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-20 13:06:59 +0100

    props header

--- src/components/footer/FooterImages.jsx
+++ src/components/footer/FooterImages.jsx
@@ -3,7 +3,7 @@ import "./Footer.css"
 const FooterImages = (props) => {
     return ( 
         <section className="footerSection">
-                    <img src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80" alt="" />
+                    <img src= {props.imgSrc} alt="" />
         <div className="footerText">
         <p className="foodName">{props.name}</p>
         <p className="foodDescription">{props.description}</p>
