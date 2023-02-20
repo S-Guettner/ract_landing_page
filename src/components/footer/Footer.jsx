bf7a4759c03818f63d89412d09c374c03912139e
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-20 12:50:48 +0100

    props footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,15 +4,16 @@ import "./Footer.css"
 const Footer = () => {
     return ( 
         <footer>
-            <FooterImages />
-            <FooterImages />
-            <FooterImages />
-            <FooterImages />
-            <FooterImages />
-            <FooterImages />
-            <FooterImages />
-            <FooterImages />
+            <FooterImages name='Salat' description='Lorem ipsum dolor sit amet.HALLLLO' price='7,50€'/>
+            <FooterImages name='Salat2' description='Lorem ipsum dolor sit amet.HALLLLO' price='8,50€' />
+            <FooterImages name='Salat3' description='Lorem ipsum dolor sit amet.HALLLLO' price='9,50€' />
+            <FooterImages name='Salat4' description='Lorem ipsum dolor sit amet.HALLLLO' price='10,50€' />
+            <FooterImages name='Salat5' description='Lorem ipsum dolor sit amet.HALLLLO' price='11,50€' />
+            <FooterImages name='Salat6' description='Lorem ipsum dolor sit amet.HALLLLO' price='13,50€' />
+            <FooterImages name='Salat7' description='Lorem ipsum dolor sit amet.HALLLLO' price='15,50€' />
+            <FooterImages name='Salat8' description='Lorem ipsum dolor sit amet.HALLLLO' price='17,50€' />
         </footer>
+        
      );
 }
  
--- src/components/footer/FooterImages.jsx
+++ src/components/footer/FooterImages.jsx
@@ -1,13 +1,13 @@
 import "./Footer.css"
 
-const FooterImages = () => {
+const FooterImages = (props) => {
     return ( 
         <section className="footerSection">
                     <img src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80" alt="" />
         <div className="footerText">
-        <p>Salat</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>7,50€</p>
+        <p className="foodName">{props.name}</p>
+        <p className="foodDescription">{props.description}</p>
+        <p className="foodPrice">{props.price}</p>
 
         </div>
         </section>
